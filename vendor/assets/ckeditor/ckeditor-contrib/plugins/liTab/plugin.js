57255e8a66063bf0ae61646e84edf89fe9c93976
dan-hoerr <dhoerr@gmail.com>
2013-05-09 17:58:44 -0400

    cleaned up tabbing
    Signed-off-by: dan-hoerr <dhoerr@gmail.com>

--- vendor/assets/ckeditor/ckeditor-contrib/plugins/liTab/plugin.js
+++ vendor/assets/ckeditor/ckeditor-contrib/plugins/liTab/plugin.js
@@ -8,17 +8,18 @@
                   {
                       var sel = editor.getSelection(),
                       ancestor = sel.getCommonAncestor();
-                      li = ancestor.getAscendant({li:1, td:1, th:1}, true);
-                      // if(li && li.$.nodeName == 'LI') {
+                      asc = ancestor.getAscendant({li:1, td:1, th:1, p:1}, true);
+
+                      if( (asc === "undefined" || asc === null) || (asc.$.nodeName != 'TD' && asc.$.nodeName != 'TH') ){ 
                         editor.execCommand(ev.data.keyCode == 9 ? 'indent' : 'outdent');
                         ev.cancel();
-                      // }
+                      }
                       // else we've found a td/th first, so let's not break the
                       // existing tab functionality in table cells.
                   }
                   
               }
-          }, null, null, 5); // high priority (before the tab plugin)
+          }, null, null, 5); // hi#gh priority (before the tab plugin)
       }
   });
 
